0e8753d98206c5bc4627435516f10eb8b2add956
rvanetta97 <rachel.vanetta@gmail.com>
2024-05-06 18:12:12 -0400

    POST method

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -15,4 +15,32 @@ router.get('/notes',(req, res)=> {
     })
 })
 
+router.post('/notes', (req, res) => {
+    fs.readFile('db/db.json', 'utf-8', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            const notes = JSON.parse(data);
+            const newNote = {
+                id: uuidv4(),
+                title: req.body.title,
+                text: req.body.text
+            };
+            
+            // Add the new note to the array of notes
+            notes.push(newNote);
+            // Write the updated array of notes back to the file
+            fs.writeFile('db/db.json', JSON.stringify(notes, null, 2), err => {
+                if (err) {
+                    console.log(err);
+                    res.status(500).send('Internal Server Error');
+                } else {
+                    res.json(newNote);
+                }
+            });
+        }
+    });
+});
+
 module.exports = router
