fd588457a927eaf4d582effd5e6c7a4ece4c3738
rvanetta97 <rachel.vanetta@gmail.com>
2024-05-05 21:29:27 -0400

    server adjustments and additional routes added

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
 const express = require('express')
 const path = require('path');
-const { middleware } = require('./middleware/middleware')
+const fs = require('fs')
+const PORT = process.env.port || 3001;
+const app = express();
+const db = require('./db/db.json')
+
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// routes to route files
+require('./routes/display')(app);
+require('./routes/notes')(app);
+
+
+// app listener - starts the server
+app.listen(PORT, () => {
+  console.log(`The digital realm awaits! Server listening on localhost:${PORT}`);
+});
